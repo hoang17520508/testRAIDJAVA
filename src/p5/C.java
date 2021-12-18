d2ab1d24c4f1652d8b69207be7aef27c0b72805e
Hoanguitk12 <luna02091999@gmail.com>
2021-12-18 08:07:20 +0700

    move

--- src/p5/C.java
+++ src/p5/C.java
@@ -1,2 +1,15 @@
 package p5;
 //sth to test
+public class A {
+    public void printNumberType(int v) {
+      if (v % 2 == 0) {
+        System.out.println("even");
+      } else {
+        System.out.println("odd");
+      }
+    }
+  
+    public void M1(int v) {
+      this.printNumberType(v);
+    }
+  }
